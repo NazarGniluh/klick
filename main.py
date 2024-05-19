d563f68b14b57c50d417447a18b6d734b321e013
vadimzhenchur <140846129+NazarGniluh@users.noreply.github.com>
2024-05-19 17:03:51 +0300

    main2

--- main.py
+++ main.py
@@ -14,9 +14,18 @@ class MainScreen(Screen):
 
 
 
+class MiniScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def Click(self):
+        self.manager.current = 'main'
+
+
 class CkickApp(App):
     def build(self):
         sm = ScreenManager()
+        sm.add_widget(MiniScreen(name='mini'))
         sm.add_widget(MainScreen(name='main'))
         return sm
 
