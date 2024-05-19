a95908927a8d968cab9cd896d9936729b641aa1b
vadimzhenchur <140846129+NazarGniluh@users.noreply.github.com>
2024-05-19 18:22:13 +0300

    play

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
+import json
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+
+player = {
+    "score": 0,
+    "power": 1
+}
+
+def read_dat():
+    global player
+    try:
+        with open("play.json", "r", encoding="utf-8") as file:
+            player = json.load(file)
+    except:
+        print("НЕВДАЧА")
+
+
+
+def save_dat():
+    global player
+    try:
+        with open("play.json", "w", encoding="utf-8") as file:
+           json.dump(player, file, indent=4, ensure_ascii=True)
+    except:
+        print("Unlucky")
 
 
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+    def on_enter(self, *args):
+        read_dat()
+        self.ids.score_lbl.text = " рахунок: " + str(player["score"])
 
     def cli(self):
         self.ids.ball.size_hint = (0.5, 0.5)
+
+        read_dat()
+        player["score"] += player["power"]
+        self.ids.score_lbl.text = " рахунок: " + str(player["score"])
+        save_dat()
 
     def cli1(self):
         self.ids.ball.size_hint = (1, 1)
